2e7146d90c35353d5ed0e6f96fba3dd67139e761
Ervin Tuzlic <ervin@fragnet.net>
2022-09-05 13:22:00 +0200

    ThemeSettings

--- src/App.js
+++ src/App.js
@@ -41,6 +41,9 @@ const App = () => {
             </div>
 
           <div>
+
+            <ThemeSettings />
+
             <Routes>
               {/* Dashboard */}
               <Route path="/" element={<Ecommerce />} />
